eaf831e611ca01ff3d1b60e2a3e16c01c807bbad
joyamandy <kyndeleon97@gmail.com>
2025-09-22 17:14:04 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,37 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+
+  devIndicators: {
+    position: "bottom-right",
+  },
+};
+
+export default withSentryConfig(nextConfig, {
+  org: "js-mastery-dk",
+  project: "javascript-nextjs",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
